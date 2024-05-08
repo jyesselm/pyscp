5f3be3e5807e119ab3b0af428b6183fd1d891fd9
Joseph Yesselman <jyesselm@unl.edu>
2024-05-07 20:39:13 -0500

    streamlined a bit more

--- pyscp/cli.py
+++ pyscp/cli.py
@@ -2,19 +2,6 @@ import click
 import os
 
 
-"""
-# multi commmand format
-@click.group()
-def cli():
-    pass
-
-
-@cli.command()
-def func1():
-    pass
-"""
-
-
 def get_remote_server_address(server):
     if server == "swan":
         return "jyesselm@swan.unl.edu"
@@ -29,36 +16,29 @@ def get_remote_server_path(server):
         raise ValueError("Server not recognized")
 
 
-@click.group()
-def cli():
-    pass
-
-
-@cli.command()
+@click.command()
 @click.argument("server")
-@click.argument("local_path")
-@click.argument("remote_path", default="")
-def put(server, local_path, remote_path):
+@click.argument("path_1")
+@click.argument("path_2", default="")
+def cli(server, path_1, path_2):
     server_address = get_remote_server_address(server)
     server_path = get_remote_server_path(server)
+    # assume get
+    if path_2 == "":
+        print("getting file: ", path_1)
+        local_path, remote_path = path_2, path_1
+    # assume put
+    else:
+        print("putting" + path_1 + " to remote path " + path_2)
+        local_path, remote_path = path_1, path_2
     if remote_path.startswith("~/"):
         remote_path = remote_path[2:]
     if not remote_path.startswith(server_path):
         remote_path = server_path + "/" + remote_path
-    os.system(f"scp -r {local_path} {server_address}:{remote_path}")
-
-
-@cli.command()
-@click.argument("server")
-@click.argument("remote")
-def get(server, remote):
-    server_address = get_remote_server_address(server)
-    server_path = get_remote_server_path(server)
-    if remote_path.startswith("~/"):
-        remote_path = remote_path[2:]
-    if not remote_path.startswith(server_path):
-        remote_path = server_path + "/" + remote_path
-    os.system(f"scp -r {server_address}:{remote_path} .")
+    if local_path == "":
+        os.system(f"scp -r {server_address}:{remote_path} .")
+    else:
+        os.system(f"scp -r {local_path} {server_address}:{remote_path}")
 
 
 if __name__ == "__main__":
